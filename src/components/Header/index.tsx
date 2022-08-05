e9d65b3e2075d09061ffccc29f09683638b40018
WillAngelis <willangelis@gmail.com>
2022-08-05 18:08:52 -0300

    add responsividade ao header e sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, IconButton, useBreakpointValue, Icon } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
@@ -6,6 +8,7 @@ import { Profile } from './Profile';
 import { Search } from './Search';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +24,16 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr='2'
+        ></IconButton>
+      )} 
       <Logo />
       {isWideVersion && <Search />}
       <Flex align="center" ml="auto">
