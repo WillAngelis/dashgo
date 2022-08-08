1db8d0f4c186c7cc65374dcc84544dc2fc9131b5
WillAngelis <willangelis@gmail.com>
2022-08-08 17:30:40 -0300

    add chamada a api users usando react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,22 +14,34 @@ import {
   Text,
   useBreakpointValue,
   IconButton,
+  Spinner,
 } from '@chakra-ui/react';
 
 import Link from 'next/link';
 
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
+import { useQuery } from '@tanstack/react-query';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 import { useEffect } from 'react';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery(['users'], async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+
+    return data;
+  });
+
+  console.log(data);
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
   return (
     <Box>
       <Header />
@@ -53,56 +65,69 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="messenger" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th w="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  {' '}
-                  <Checkbox colorScheme="messenger" />{' '}
-                </Td>
-                <Td px={['4', '4', '6']}>
-                  <Box>
-                    <Text fontWeight="bold">William Angelis</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      willangelis@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>05 de Agosto, 2022</Td>}
-                <Td>
-                  {isWideVersion ? (
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="messenger"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
-                      cursor="pointer"
-                    >
-                      Editar
-                    </Button>
-                  ) : (
-                    <IconButton
-                      aria-label="Edit"
-                      colorScheme="messenger"
-                      icon={<Icon as={RiPencilLine} fontSize="16" />}
-                    />
-                  )}
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="messenger" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th w="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      {' '}
+                      <Checkbox colorScheme="messenger" />{' '}
+                    </Td>
+                    <Td px={['4', '4', '6']}>
+                      <Box>
+                        <Text fontWeight="bold">William Angelis</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          willangelis@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>05 de Agosto, 2022</Td>}
+                    <Td>
+                      {isWideVersion ? (
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="messenger"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
+                          cursor="pointer"
+                        >
+                          Editar
+                        </Button>
+                      ) : (
+                        <IconButton
+                          aria-label="Edit"
+                          colorScheme="messenger"
+                          icon={<Icon as={RiPencilLine} fontSize="16" />}
+                        />
+                      )}
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
