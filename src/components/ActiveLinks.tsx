db6c5a9ce52675f1ff69d1d2b7e27b3b8a8423ed
WillAngelis <willangelis@gmail.com>
2022-08-08 18:03:18 -0300

    change active links color

--- src/components/ActiveLinks.tsx
+++ src/components/ActiveLinks.tsx
@@ -34,7 +34,7 @@ export function ActiveLink({
   return (
     <Link {...rest}>
       {cloneElement(children, {
-        color: isActive ? '#0078FF' : 'gray.50',
+        color: isActive ? 'pink.500' : 'gray.50',
       })}
     </Link>
   );
