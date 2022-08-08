91533b1dafc1907ac89e520a2ec940fd17c05d0b
WillAngelis <willangelis@gmail.com>
2022-08-08 15:54:10 -0300

    add validação no input

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,31 +1,41 @@
+import React from 'react';
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from '@chakra-ui/react';
+import { FieldError } from 'react-hook-form';
 
 interface InputProps extends ChakraInputProps {
-  name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
-  return (
-    <FormControl>
-      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-      <ChakraInput
-        name={name}
-        id={name}
-        focusBorderColor="cyan.500"
-        bgColor="gray.900"
-        variant="filled"
-        _hover={{
-          bgColor: 'gray.900',
-        }}
-        size="lg"
-        {...rest}
-      />
-    </FormControl>
-  );
-}
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error = null, ...props }, ref) => {
+    return (
+      <FormControl isInvalid={!!error}>
+        {!!label && <FormLabel htmlFor="password">{label}</FormLabel>}
+
+        <ChakraInput
+          {...props}
+          id={props.name}
+          ref={ref}
+          focusBorderColor="cyan.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{ bgColor: 'gray.900' }}
+          size="lg"
+        />
+
+        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      </FormControl>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+export default Input;
