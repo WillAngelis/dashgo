b81ba950e29579dd46e82ccc8d45352637e7e07d
WillAngelis <willangelis@gmail.com>
2022-08-09 12:27:49 -0300

    add loading quando estiver em refecth

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -35,26 +35,7 @@ type User = {
 };
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(['users'], async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
-
-    const users = data.users.map((user: User) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createAt: new Date(user.createAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
-      };
-    });
-    return users;
-  });
-
-  console.log(data);
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -70,6 +51,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4"></Spinner>
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
